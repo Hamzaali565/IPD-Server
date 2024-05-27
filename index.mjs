dcdd9bcb14ac0bf57b66db560ffd4118fc36f81d
Hamzaali565 <ha6153806@gmail.com>
2024-05-27 12:42:51 +0500

    setup completed

---
+++ index.mjs
@@ -0,0 +1,62 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+mongoose.set("strictQuery", false);
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://lab-model-v2.vercel.app"],
+    credentials: true,
+  })
+);
+
+const __dirname = path.resolve();
+app.use("/", express.static(path.join(__dirname, "./Frontend/build")));
+app.use("*", express.static(path.join(__dirname, "./Frontend/build")));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+mongoose.connect(MONGODB_URI);
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on("connected", function () {
+  //connected
+  console.log("Database is connected");
+});
+
+mongoose.connection.on("disconnected", function () {
+  //disconnected
+  console.log("Mongoose is disconnected");
+  process.exit(1);
+});
+
+mongoose.connection.on("error", function (err) {
+  //any error
+  console.log("Mongoose connection error: ", err);
+  process.exit(1);
+});
+
+process.on("SIGINT", function () {
+  /////this function will run just before the app is closing
+  console.log("app is terminating");
+  mongoose.connection.close(); // Remove the callback function here
+  console.log("Mongoose default connection closed");
+  process.exit(0);
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
