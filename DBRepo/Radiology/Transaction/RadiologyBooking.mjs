448db5ad8d9d72901f4d92dbd6882b87d918069b
Hamzaali565 <ha6153806@gmail.com>
2024-07-11 03:12:35 +0500

    Radiology schema created ... working on api is required

---
+++ DBRepo/Radiology/Transaction/RadiologyBooking.mjs
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+import AutoIncrementFactory from "mongoose-sequence";
+
+const AutoIncrement = AutoIncrementFactory(mongoose.connection);
+
+const radiologyBooking = new mongoose.Schema({
+  radiologyNo: { type: Number, unique: true },
+  mrNo: { type: String, required: true },
+  consultant: { type: String, required: true },
+  party: { type: String, required: true },
+  remarks: { type: String },
+  amount: { type: Number, required: true },
+  paymentType: { type: String, required: true },
+  location: { type: String, required: true },
+  isDeleted: { type: Boolean, default: false },
+  deletedUser: { type: String },
+  deletedOn: { type: String },
+  serviceDetails: [
+    {
+      serviceName: { type: String, required: true },
+      quantity: { type: String, required: true },
+      amount: { type: String, required: true },
+      serviceId: { type: mongoose.ObjectId },
+      _id: { type: String, default: uuidv4 },
+    },
+  ],
+  createdUser: { type: String, required: true },
+  createdDate: { type: String, required: true },
+});
+
+radiologyBooking.plugin(AutoIncrement, { inc_field: "radiologyNo" });
+
+export const RadiologyBookingModel = mongoose.model(
+  "RadiologyBookingModel",
+  radiologyBooking
+);
