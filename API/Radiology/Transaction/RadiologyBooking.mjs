249ee7b0d976505f5b5b6050924fb6608b987363
Hamzaali565 <ha6153806@gmail.com>
2024-07-15 13:05:59 +0500

    finally push data from fronend is required

--- API/Radiology/Transaction/RadiologyBooking.mjs
+++ API/Radiology/Transaction/RadiologyBooking.mjs
@@ -96,7 +96,9 @@ router.get("/radiologyreverse", async (req, res) => {
     if (response.length <= 0)
       throw new Error("NO SERVICES ADDED TO THIS PATIENT!!!");
     const flatData = response.flatMap((item) => item?.serviceDetails);
-    const updatedData = flatData.filter((items) => items?.isDeleted !== false);
+    const updatedData = flatData.filter(
+      (items) => items?.isDeleted !== false && items?.refund !== true
+    );
     res.status(200).send({ data: updatedData });
   } catch (error) {
     res.status(400).send({ message: error.message });
